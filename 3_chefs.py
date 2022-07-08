016b4890512c577398c5ad5ee39ff31add8889f3
Mariusz Leśniak <lesniak.mar@gmail.com>
2022-07-08 16:36:17 +0200

    Mission solved using factory design pattern.

--- 3_chefs.py
+++ 3_chefs.py
@@ -1,19 +1,38 @@
-class AbstractCook:
-    pass
+from abc import ABC, abstractmethod
 
-class JapaneseCook(AbstractCook):
-    pass
 
-class RussianCook(AbstractCook):
-    pass
+class CookFactory(ABC):
+    def __init__(self):
+        self.food_cost = 0
+        self.drink_cost = 0
 
-class ItalianCook(AbstractCook):
-    pass
+    def add_food(self, amount: int, price: float) -> None:
+        self.food_cost = self.food_cost + amount * price
+
+    def add_drink(self, amount: int, price: float) -> None:
+        self.drink_cost = self.drink_cost + amount * price
+
+    @abstractmethod
+    def total(self):
+        pass
+
+
+class JapaneseCook(CookFactory):
+    def total(self) -> str:
+        return f"Sushi: {self.food_cost}, Tea: {self.drink_cost}, Total: {self.drink_cost+self.food_cost}"
+
+
+class RussianCook(CookFactory):
+    def total(self) -> str:
+        return f"Dumplings: {self.food_cost}, Compote: {self.drink_cost}, Total: {self.drink_cost + self.food_cost}"
+
+
+class ItalianCook(CookFactory):
+    def total(self) -> str:
+        return f"Pizza: {self.food_cost}, Juice: {self.drink_cost}, Total: {self.drink_cost + self.food_cost}"
 
 
 if __name__ == '__main__':
-    #These "asserts" using only for self-checking and not necessary for auto-testing
-
     client_1 = JapaneseCook()
     client_1.add_food(2, 30)
     client_1.add_food(3, 15)
@@ -32,4 +51,3 @@ if __name__ == '__main__':
     assert client_1.total() == "Sushi: 105, Tea: 20, Total: 125"
     assert client_2.total() == "Dumplings: 90, Compote: 100, Total: 190"
     assert client_3.total() == "Pizza: 100, Juice: 20, Total: 120"
-    print("Coding complete? Let's try tests!")
